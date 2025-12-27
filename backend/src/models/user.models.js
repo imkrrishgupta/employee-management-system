ae99e6df6c4c2c96d348ef67b30f69d82a43f83c
Krrish Kumar <imkrrishgupta@gmail.com>
2025-12-28 01:25:15 +0530

    update functionality for saving hashed password

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -37,4 +37,10 @@ const userSchema = new Schema(
     { timestamps: true }
 );
 
+userSchema.pre("save", async function(){
+    if (!this.isModified("password")) return;
+
+    this.password = await bcrypt.hash(this.password, Number(`${process.env.ENCRYPTION_ROUND}`));
+})
+
 export const User = mongoose.model("User", userSchema);
--- backend/src/seeders/admin.seeders.js
+++ backend/src/seeders/admin.seeders.js
@@ -1,17 +1,14 @@
 import { User } from "../models/user.models.js";
 import { connectDB } from "../db/index.js";
-import bcrypt from "bcrypt";
 
 const userRegister = async () => {
     try {
         await connectDB();
 
-        const hashPassword = await bcrypt.hash(`${process.env.ADMIN_PASS}`, Number(process.env.ENCRYPTION_ROUND));
-
         await User.create({
             name: "Admin",
             email: `${process.env.ADMIN_EMAIL}`,
-            password: hashPassword,
+            password: `${process.env.ADMIN_PASS}`,
             role: "admin"
         });
 
