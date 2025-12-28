2c3e3c180a5393be3ba641b815b5e252faa30f2e
Krrish Kumar <imkrrishgupta@gmail.com>
2025-12-29 00:54:03 +0530

    add functionality for generating access and refresh token

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -47,4 +47,23 @@ userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password);
 }
 
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id: this._id,
+        role: this.role,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    );
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }  // We will make a database query if the `refeshToken` is not present we will not allow the `accessToken` to get refreshed and will forcefully logout the user
+    );
+}
+
 export const User = mongoose.model("User", userSchema);
