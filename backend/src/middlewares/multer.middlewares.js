e0b4bc1753680d5a6dfbbeb1c98ccf1bf3dd0b12
Krrish Kumar <imkrrishgupta@gmail.com>
2025-12-29 00:27:09 +0530

    add middlewares required

---
+++ backend/src/middlewares/multer.middlewares.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+export const uploadAvatar = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 2 * 1024 * 1024,  // 2MB
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only image files allowed"), false);
+    }
+  },
+});
