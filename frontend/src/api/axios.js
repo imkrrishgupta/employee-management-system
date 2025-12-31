e730ca3dff1f6effb5a4e0d4b8df5d7c412fe804
Krrish Kumar <imkrrishgupta@gmail.com>
2025-12-31 20:13:18 +0530

    update api for base url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const baseURL =
-  import.meta?.env?.VITE_BASE_URL || "http://localhost:5000/api/v1";
+const baseURL = import.meta.env.VITE_BASE_URL;
 
 const api = axios.create({
   baseURL,
-  withCredentials: true,
+  withCredentials: true
 });
 
 export default api;
