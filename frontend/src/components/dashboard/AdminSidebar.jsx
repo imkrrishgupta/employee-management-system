8a59150665146d5d2648405d7789b15fdffc75aa
Krrish Kumar <imkrrishgupta@gmail.com>
2026-01-08 13:53:22 +0530

    update ui

--- frontend/src/components/dashboard/AdminSidebar.jsx
+++ frontend/src/components/dashboard/AdminSidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { FaTachometerAlt } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
 import { FaBuilding } from "react-icons/fa";
@@ -10,45 +10,47 @@ import { FaCogs } from "react-icons/fa";
 const AdminSidebar = () => {
   return (
     <div className='bg-gray-800 text-white h-screen fixed left-0 top-0 bottom-0 space-y-2 w-64'>
+        <Link to='/admin-dashboard'>
         
-        <div className='h-12 flex items-center justify-center'>
-            <h3 className='font-serif text-2xl text-centre'>Employee MS</h3>
-        </div>
+            <div className='h-12 flex items-center justify-center border-b border-gray-600'>
+                <h3 className='font-serif text-2xl text-centre'>Employee MS</h3>
+            </div>
 
-        <div>
+            <div>
             
-            <NavLink to='/admin-dashboard' className={({isActive}) => `${isActive ? "bg-[#373A8F]" : " "} flex items-center space-x-4 py-2.5 px-4 rounded-lg`} end>
-                <FaTachometerAlt />
-                <span>Dashboard</span>
-            </NavLink>
+                <NavLink to='/admin-dashboard' className={({isActive}) => `${isActive ? "bg-[#373A8F]" : " "} flex items-center space-x-4 py-2.5 px-4 rounded-lg`} end>
+                    <FaTachometerAlt />
+                    <span>Dashboard</span>
+                </NavLink>
 
-            <NavLink to='/admin-dashboard' className='flex items-center space-x-4 py-2.5 px-4 rounded-lg'>
-                <FaUser />
-                <span>Employees</span>
-            </NavLink>
+                <NavLink to='/admin-dashboard' className='flex items-center space-x-4 py-2.5 px-4 rounded-lg'>
+                    <FaUser />
+                    <span>Employees</span>
+                </NavLink>
 
-            <NavLink to='/admin-dashboard/departments' className={({isActive}) => `${isActive ? "bg-[#373A8F]" : " "} flex items-center space-x-4 py-2.5 px-4 rounded-lg`}>
-                <FaBuilding />
-                <span>Departments</span>
-            </NavLink>
+                <NavLink to='/admin-dashboard/departments' className={({isActive}) => `${isActive ? "bg-[#373A8F]" : " "} flex items-center space-x-4 py-2.5 px-4 rounded-lg`}>
+                    <FaBuilding />
+                    <span>Departments</span>
+                </NavLink>
 
-            <NavLink to='/admin-dashboard' className='flex items-center space-x-4 py-2.5 px-4 rounded-lg'>
-                <FaCalendarAlt />
-                <span>Leaves</span>
-            </NavLink>
+                <NavLink to='/admin-dashboard' className='flex items-center space-x-4 py-2.5 px-4 rounded-lg'>
+                    <FaCalendarAlt />
+                    <span>Leaves</span>
+                </NavLink>
 
-            <NavLink to='/admin-dashboard' className='flex items-center space-x-4 py-2.5 px-4 rounded-lg'>
-                <FaMoneyBillWave />
-                <span>Salary</span>
-            </NavLink>
+                <NavLink to='/admin-dashboard' className='flex items-center space-x-4 py-2.5 px-4 rounded-lg'>
+                    <FaMoneyBillWave />
+                    <span>Salary</span>
+                </NavLink>
 
-            <NavLink to='/admin-dashboard' className='flex items-center space-x-4 py-2.5 px-4 rounded-lg'>
-                <FaCogs />
-                <span>Settings</span>
-            </NavLink>
+                <NavLink to='/admin-dashboard' className='flex items-center space-x-4 py-2.5 px-4 rounded-lg'>
+                    <FaCogs />
+                    <span>Settings</span>
+                </NavLink>
 
-        </div>
-
+            </div>
+            
+        </Link>
     </div>
   )
 }
--- frontend/src/components/dashboard/AdminSummary.jsx
+++ frontend/src/components/dashboard/AdminSummary.jsx
@@ -9,7 +9,7 @@ const AdminSummary = () => {
 
         <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-6'>
             <SummaryCard icon={<FaUsers />} text="Total Employees" number={15} bgColor="bg-linear-to-b from-indigo-200 to-indigo-500" />
-            <SummaryCard icon={<FaBuilding />} text="Total Departments" number={5} bgColor="bg-linear-to-b from-yellow-200 to-yellow-500" />
+            <SummaryCard icon={<FaBuilding />} text="Total Departments" number={5} bgColor="bg-linear-to-b from-amber-200 to-amber-500" />
             <SummaryCard icon={<FaMoneyBillWave />} text="Monthly Salary" number="₹200000" bgColor="bg-linear-to-b from-red-200 to-red-500" />
         </div>
 
@@ -19,7 +19,7 @@ const AdminSummary = () => {
             <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-6'>
                 <SummaryCard icon={<FaFileAlt />} text="Leave Applied" number={5} bgColor="bg-linear-to-b from-indigo-200 to-indigo-500" />
                 <SummaryCard icon={<FaCheckCircle />} text="Leave Approved" number={2} bgColor="bg-linear-to-b from-green-200 to-green-500" />
-                <SummaryCard icon={<FaHourglassHalf />} text="Leave Pending" number={4} bgColor="bg-linear-to-b from-yellow-200 to-yellow-500" />
+                <SummaryCard icon={<FaHourglassHalf />} text="Leave Pending" number={4} bgColor="bg-linear-to-b from-amber-200 to-amber-500" />
                 <SummaryCard icon={<FaTimesCircle />} text="Leave Rejected" number={1} bgColor="bg-linear-to-b from-red-200 to-red-500" />
             </div>
             
