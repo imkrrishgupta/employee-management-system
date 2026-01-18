4b508ccbaecc6bc4829ede482f4c6aa61305fc55
Krrish Kumar <imkrrishgupta@gmail.com>
2026-01-18 18:12:11 +0530

    update ui for navbar

--- frontend/src/components/dashboard/Navbar.jsx
+++ frontend/src/components/dashboard/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between h-12 bg-gray-300 px-5'>
         
-        <p>Welcome {user.name}</p>
+        <p className="text-lg font-semibold text-gray-800">Welcome {user.name}</p>
 
         <button 
           className='text-white px-4 py-1 bg-red-500 hover:bg-red-600 rounded-lg'
